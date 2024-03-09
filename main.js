2cd01a0cf187e548a838e50977f13597c1b019f8
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-09 15:04:44 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+// Question no # 13     
+let cars = ['honda', 'honda motorcyycle', 'bmw', 'ferrari'];
+for (let car of cars) {
+    console.log(`I would like to own ${car}`);
+}
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let number of numbers) {
+    console.log(number);
+}
---
+++ main.ts
@@ -0,0 +1,12 @@
+// Question no # 13     
+
+let cars: string[] = ['honda', 'honda motorcyycle', 'bmw', 'ferrari'];
+
+for(let car of cars){
+    console.log(`I would like to own ${car}`);
+}
+
+let numbers: number[] = [1,2,3,4,5,6,7,8,9];
+for(let number of numbers){
+    console.log(number);
+}
